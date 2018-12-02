71a63e47c1a6fee2fbef3c754f37b13b45b25a04
quak <babaeva.irina@notissimus.com>
2018-12-02 16:57:13 +0300

    Added loading of images

--- iOS/Views/Cells/DataCollectionViewCell.cs
+++ iOS/Views/Cells/DataCollectionViewCell.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using CoreGraphics;
+using FFImageLoading.Cross;
 using Foundation;
 using MovieApp.Core.ViewModels;
 using MvvmCross.Binding.BindingContext;
@@ -13,20 +14,14 @@ namespace MovieApp.iOS.Views.Cells
         public static readonly NSString Key = new NSString("DataCollectionViewCell");
         public static readonly UINib Nib = UINib.FromName("DataCollectionViewCell", NSBundle.MainBundle);
 
-        public override bool Selected
-        {
-            get
-            {
-                return base.Selected;
-            }
-            set
-            {
-                base.Selected = value;
+        private MvxCachedImageView _imageControl;
 
-                if (DescriptionLabel != null)
-                    DescriptionLabel.Highlighted = value;
-            }
+        static DataCollectionViewCell()
+        {
+
         }
+
+        #region Initialize
 
         protected void InitializeControls()
         {
@@ -37,7 +32,14 @@ namespace MovieApp.iOS.Views.Cells
 
         private void SetupIcon(UIImageView iconImageView)
         {
+            // TODO: исправить этот костыль
+            // надо создать свой контрол и его добавить в xib
+            // иначе при изменении размера все уедет
+            // И вообще у меня тут картинка внутри картинки и это ужасно, надо исправить
+            // Но пока нет времени
+            _imageControl = new MvxCachedImageView(new CGRect(0, 0, 23, 40));
 
+            _imageControl.BackgroundColor = UIColor.Red;
         }
 
         protected void SetupDescription(UILabel description)
@@ -45,20 +47,34 @@ namespace MovieApp.iOS.Views.Cells
 
         }
 
+        #endregion
+
+        #region Binding
+
         protected void BindControls()
         {
             var set = this.CreateBindingSet<DataCollectionViewCell, IDataItemVM>();
 
-            set.Bind(DescriptionLabel).To(vm => vm.Title);
-            set.Bind(IconImageView).For(i => i.Image).To(vm => vm.PosterPath);
+            BindingDescriptionLabel(DescriptionLabel, set);
+
+            BindingIconImage(IconImageView, set);
 
             set.Apply();
         }
 
-        static DataCollectionViewCell()
+        private void BindingDescriptionLabel(UILabel descriptionLabel, MvxFluentBindingDescriptionSet<DataCollectionViewCell, IDataItemVM> set)
         {
-            //Nib = UINib.FromName("DataCollectionViewCell", NSBundle.MainBundle);
+            set.Bind(descriptionLabel).To(vm => vm.Title);
         }
+
+        private void BindingIconImage(UIImageView iconImageView, MvxFluentBindingDescriptionSet<DataCollectionViewCell, IDataItemVM> set)
+        {
+            set.Bind(_imageControl).For(v => v.ImagePath).To(vm => vm.PosterPath);
+
+            iconImageView.AddSubview(_imageControl);
+        }
+
+        #endregion
 
         protected DataCollectionViewCell(IntPtr handle) : base(handle)
         {
@@ -68,6 +84,5 @@ namespace MovieApp.iOS.Views.Cells
                 BindControls();
             });
         }
-
     }
 }
--- iOS/Views/DataDetailsViewController.cs
+++ iOS/Views/DataDetailsViewController.cs
@@ -4,11 +4,17 @@ using MvvmCross.Binding.BindingContext;
 using MvvmCross.Binding.iOS.Views;
 using MvvmCross.iOS.Views;
 using UIKit;
+//using FFImageLoading.MvvmCross.Sample.Core;
+using FFImageLoading.Cross;
+using System.Linq.Expressions;
+using CoreGraphics;
 
 namespace MovieApp.iOS.Views
 {
     public partial class DataDetailsViewController : MvxViewController<IDataDetailsViewModel>
     {
+        private MvxCachedImageView _imageControl;
+
         public DataDetailsViewController() : base("DataDetailsViewController", null)
         {
 
@@ -25,12 +31,19 @@ namespace MovieApp.iOS.Views
             SetupDescriptionLabel(DescriptionLabel);
         }
 
-        private void SetupDescriptionLabel(UILabel descriptionLabel)
+        private void SetupImageView(UIImageView descriptionImageView)
         {
+            // TODO: исправить этот костыль
+            // надо создать свой контрол и его добавить в xib
+            // иначе при изменении размера все уедет
+            // И вообще у меня тут картинка внутри картинки и это ужасно, надо исправить
+            // Но пока нет времени
+            _imageControl = new MvxCachedImageView(new CGRect(0, 0, 200, 300));
 
+            _imageControl.BackgroundColor = UIColor.Red;
         }
 
-        private void SetupImageView(UIImageView descriptionImageView)
+        private void SetupDescriptionLabel(UILabel descriptionLabel)
         {
 
         }
@@ -50,14 +63,16 @@ namespace MovieApp.iOS.Views
             set.Apply();
         }
 
+        private void BindDescriptionImageView(UIImageView descriptionImageView, MvxFluentBindingDescriptionSet<DataDetailsViewController, IDataDetailsViewModel> set)
+        {
+            set.Bind(_imageControl).For(v => v.ImagePath).To(vm => vm.MovieItem.PosterPath);
+
+            descriptionImageView.AddSubview(_imageControl);
+        }
+
         private void BindDescriptionLabel(UILabel descriptionLabel, MvxFluentBindingDescriptionSet<DataDetailsViewController, IDataDetailsViewModel> set)
         {
             set.Bind(descriptionLabel).To(vm => vm.MovieItem.Description);
-        }
-
-        private void BindDescriptionImageView(UIImageView descriptionImageView, MvxFluentBindingDescriptionSet<DataDetailsViewController, IDataDetailsViewModel> set)
-        {
-            set.Bind(descriptionImageView).To(vm => vm.MovieItem.PosterPath);
         }
 
         #endregion
