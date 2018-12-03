b7f25ff71b189103aa5a04ae5da1e2beacb86e44
quak <babaeva.irina@notissimus.com>
2018-12-03 04:24:12 +0300

    Let's go testing

--- TestMovieApp/Test.cs
+++ TestMovieApp/Test.cs
@@ -1,13 +1,25 @@
 ﻿using NUnit.Framework;
 using System;
+using MovieApp.Core.ViewModels;
+using System.Linq;
+using System.Text;
+
 namespace TestMovieApp
 {
-    [TestFixture()]
+    [TestFixture]
     public class Test
     {
-        [Test()]
-        public void TestCase()
+        [Test]
+        // Проверяю, что метод добавления элемента в историю выполняется без ошибок
+        public void SaveToHistoryTest()
         {
+            var viewModel = new DataViewModel();
+
+            StringBuilder desc = new StringBuilder();
+            desc.AppendLine("test description");
+            var testItem = new DataItemVM("id", "title", "url", desc);
+
+            Assert.DoesNotThrow(() => viewModel.SaveInfoInHistory(testItem));
         }
     }
 }
